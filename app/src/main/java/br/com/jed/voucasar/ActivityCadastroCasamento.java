a919134c143a7196ed0975cbc97f6c883b954bab
Everdes <e_verde_s@hotmail.com>
2015-11-04 00:37:02 -0300

    Melhorando as telas de Cadastro de Casamento e Presentes

--- app/src/main/java/br/com/jed/voucasar/ActivityCadastroCasamento.java
+++ app/src/main/java/br/com/jed/voucasar/ActivityCadastroCasamento.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,23 +17,32 @@ public class ActivityCadastroCasamento extends AppCompatActivity {
 
     public static final int RESULT_LOAD_IMAGE = 1;
 
-    private ImageButton btnVoltar, imgBtnFotoCasal;
+    private ImageButton btnVoltar;
     private ImageView imgVwFotoCasal;
-    private Uri imageUri;
+    private FloatingActionButton fabFotoCasal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_casamento);
 
-        imgVwFotoCasal = (ImageView) findViewById(R.id.imgVwFotoCasal);
-        imgVwFotoCasal.setOnClickListener(new View.OnClickListener() {
+        fabFotoCasal = (FloatingActionButton) findViewById(R.id.fabFotoCasal);
+        fabFotoCasal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent itAbrirGaleria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(itAbrirGaleria, RESULT_LOAD_IMAGE);
             }
         });
+
+        imgVwFotoCasal = (ImageView) findViewById(R.id.imgVwFotoCasal);
+//        imgVwFotoCasal.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent itAbrirGaleria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(itAbrirGaleria, RESULT_LOAD_IMAGE);
+//            }
+//        });
 
         btnVoltar = (ImageButton) findViewById(R.id.btnVoltar);
         btnVoltar.setOnClickListener(new View.OnClickListener() {
@@ -62,9 +72,9 @@ public class ActivityCadastroCasamento extends AppCompatActivity {
 //            casamento.setFoto(caminhoArquivo);
 
             Bitmap imagem = BitmapFactory.decodeFile(diretorioImagem);
-            Bitmap fotoReduzida = Bitmap.createScaledBitmap(imagem, 100, 100, true);
+            Bitmap fotoReduzida = Bitmap.createScaledBitmap(imagem, 1000, 700, true);
             imgVwFotoCasal.setImageBitmap(fotoReduzida);
-            imgVwFotoCasal.setAdjustViewBounds(true);
+//            imgVwFotoCasal.setAdjustViewBounds(true);
         }
     }
 }
